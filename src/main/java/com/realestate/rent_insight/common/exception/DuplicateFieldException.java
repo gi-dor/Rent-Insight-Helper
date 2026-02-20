d2251123a18e13d5048e18e7f9718b2c187a9555
gi-dor <rltjs3563@naver.com>
2026-02-20 21:40:27 +0900

    중복 체크 Exception

--- src/main/java/com/realestate/rent_insight/common/exception/DuplicateFieldException.java
+++ src/main/java/com/realestate/rent_insight/common/exception/DuplicateFieldException.java
@@ -3,14 +3,19 @@ package com.realestate.rent_insight.common.exception;
 import lombok.Getter;
 
 
-// 회원가입시 발생하는 중복체크 때문에 만듬 - 나중에 로그 기록 남겨서 그래프로 조회해야징
+// 회원가입시 발생하는 id(이메일)와 닉네임 설정시 중복체크 때문에 만듬 - 나중에 로그 기록 남겨서 그래프로 조회해야징
 @Getter
 public class DuplicateFieldException extends RuntimeException{
 
     private final String field; // 중복이 발생한 필드명 (예: "email", "nickname")
 
     public DuplicateFieldException(String field, String message) {
-        super(message);
+        super(message); // 예외가 발생하면 생성자로 인해 예외 메시지가 전달된다
         this.field = field;
     }
+    /**
+     * Duplicate ~~~
+     * field : "email"
+     * message : "이미 사용중인 이메일 "
+     */
 }
