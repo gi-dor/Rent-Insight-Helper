67fb6f79df3451c87fde79da71664c48fae737b7
gi-dor <rltjs3563@naver.com>
2026-02-21 00:40:37 +0900

    delete and insert

--- src/main/java/com/realestate/rent_insight/domain/repository/RentCompleteRepository.java
+++ src/main/java/com/realestate/rent_insight/domain/repository/RentCompleteRepository.java
@@ -2,6 +2,9 @@ package com.realestate.rent_insight.domain.repository;
 
 import com.realestate.rent_insight.domain.entity.RentComplete;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RentCompleteRepository extends JpaRepository<RentComplete, Long> {
 
@@ -43,4 +46,8 @@ public interface RentCompleteRepository extends JpaRepository<RentComplete, Long
      */
    // List<RentContract> findByMonthlyRentBetween(Integer minRent, Integer maxRent);
 
+
+    @Modifying  // INSERT , UPDATE , DELETE 에서 사용하는 어노테이션이라고함
+    @Query("DELETE FROM RentComplete rc WHERE FUNCTION('DATE_FORMAT', rc.contractDate, '%Y%m') = :dealYmd")
+    void deleteAllByDealYmd(@Param("dealYmd") String dealYmd);
 }
