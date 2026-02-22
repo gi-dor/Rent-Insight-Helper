4425fca3d16ae3305fd7dc3e4c4acda5e9a49700
gi-dor <rltjs3563@naver.com>
2026-02-22 20:17:31 +0900

    엑셀파일에서 서울특별시 정보만 걸러내서 INSERT 문 만들기

--- src/main/java/com/realestate/rent_insight/DataSqlGenerator.java
+++ src/main/java/com/realestate/rent_insight/DataSqlGenerator.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
+ * 국토교통부 법정동코드 원본 CSV 파일에서, 우리가 필요로 하는 '서울특별시'의 지역 데이터만 INSERT 하기
  * '법정동코드.csv' 파일을 읽기
  * 서울시에 해당하는 지역 데이터의 SQL INSERT 구문을 생성
  */
@@ -16,7 +17,7 @@ public class DataSqlGenerator {
         String filePath = "C:\\Users\\Admin\\Downloads\\국토교통부_전국 법정동_20250807.csv";
         // 출력되는 모든 INSERT 구문을 복사
 
-        // data.sql 파일에 집어넣으면 Spring Boot 에서 뚝딱 Insert 해줌
+        // .sql 파일에 집어넣으면 Spring Boot 에서 뚝딱 Insert 해줌
 
         // ====================================================================================
 
@@ -25,18 +26,19 @@ public class DataSqlGenerator {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
 
+            // 엑셀 첫번째 줄은 컬럼명이기에 한줄 넘기려고 사용해씀둥
             br.readLine();
 
             // 파일을 한 줄씩 읽기
             while ((line = br.readLine()) != null) {
-                // 각 줄을 쉼표(,)로 나누기
+                // 각 줄을 쉼표(,)로 나누기 "1111000000,서울특별시,종로구,"
                 String[] columns = line.split(",");
 
                 // 컬럼 순서에 맞게 데이터를 변수에 담음
                 String code = columns[0];
                 String sido = columns[1];
-                String sigungu = (columns.length > 2) ? columns[2] : "";
-                String eupmyeondong = (columns.length > 3) ? columns[3] : "";
+                String sigungu = (columns.length > 2) ? columns[2] : "";        // 1100000000,서울특별시,
+                String eupmyeondong = (columns.length > 3) ? columns[3] : "";   // 1111000000,서울특별시,종로구,xxxx
 
                 // 서울특별시 데이터만 골라내기 -- csv 파일은 모든 전국의 시가 있어서 서울시만 걸러야함
                 if (sido.equals("서울특별시")) {
