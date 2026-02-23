31edec24d1014eea66876a4ee6ca956307637501
gi-dor <rltjs3563@naver.com>
2026-02-23 21:52:34 +0900

    sggCode -> sggCd umdName -> umdNm

--- src/main/java/com/realestate/rent_insight/dto/RentCompleteSearchDTO.java
+++ src/main/java/com/realestate/rent_insight/dto/RentCompleteSearchDTO.java
@@ -4,17 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * 전월세 계약의 복합 조건 검색을 위한 데이터 전송 객체(DTO)입니다.
+ * 전월세 계약의 복합 조건 검색
+ *  걍 사용자가 입력한 검색조건
  */
 @Getter
 @Setter
 public class RentCompleteSearchDTO {
 
     // 시군구 이름 대신 코드로 검색하기 위해  변경
-    private String sggCode;     // 시군구 코드 (예: "1111000000")
+    private String sggCd;     // 시군구 코드 (예: "1111000000")
 
     // 법정동 이름
-    private String umdName;     // 법정동명 (예: "청운동")
+    private String umdNm;     // 법정동명 (예: "청운동")
 
     // 단지명
     private String name;        // 오피스텔 단지명 (부분 검색용)
@@ -26,4 +27,9 @@ public class RentCompleteSearchDTO {
     // 월세
     private Integer minRent;    // 최소 월세 (단위: 만원)
     private Integer maxRent;    // 최대 월세 (단위: 만원)
+
+    private int limit;
+    private int offset;
+
+
 }
